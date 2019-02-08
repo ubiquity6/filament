7ebe46f7c742c5e531c87b04896e4352b41b597f
Aleksander Migul <aleksander@ubiquity6.com>
2019-02-08 11:21:50 -0800

    Filament test code moved to dedicated component

--- android/samples/helloreact/App.js
+++ android/samples/helloreact/App.js
@@ -180,98 +180,6 @@ function testBindings()
 
 }
 
-class HelloTriangle {
-  constructor() {
-      //this.canvas = canvas;
-      //const engine = this.engine = Filament.Engine.create(this.canvas);
-      const engine = this.engine = Filament.Engine.create();
-      this.scene = engine.createScene();
-      this.triangle = Filament.EntityManager.get().create();
-      this.scene.addEntity(this.triangle);
-
-      const TRIANGLE_POSITIONS = new Float32Array([
-          1, 0,
-          Math.cos(Math.PI * 2 / 3), Math.sin(Math.PI * 2 / 3),
-          Math.cos(Math.PI * 4 / 3), Math.sin(Math.PI * 4 / 3),
-      ]);
-
-      const TRIANGLE_COLORS = new Uint32Array([0xffff0000, 0xff00ff00, 0xff0000ff]);
-
-      this.vb = Filament.VertexBuffer.Builder()
-          .vertexCount(3)
-          .bufferCount(2)
-          .attribute(VertexAttribute.POSITION, 0, AttributeType.FLOAT2, 0, 8)
-          .attribute(VertexAttribute.COLOR, 1, AttributeType.UBYTE4, 0, 4)
-          .normalized(VertexAttribute.COLOR)
-          .build(engine);
-
-      this.vb.setBufferAt(engine, 0, TRIANGLE_POSITIONS);
-      this.vb.setBufferAt(engine, 1, TRIANGLE_COLORS);
-
-      this.ib = Filament.IndexBuffer.Builder()
-          .indexCount(3)
-          .bufferType(Filament.IndexBuffer$IndexType.USHORT)
-          .build(engine);
-
-      this.ib.setBuffer(engine, new Uint16Array([0, 1, 2]));
-
-      const mat = engine.createMaterial('nonlit.filamat');
-      const matinst = mat.getDefaultInstance();
-      Filament.RenderableManager.Builder(1)
-          .boundingBox([[ -1, -1, -1 ], [ 1, 1, 1 ]])
-          .material(0, matinst)
-          .geometry(0, Filament.RenderableManager$PrimitiveType.TRIANGLES, this.vb, this.ib)
-          .build(engine, this.triangle);
-
-      this.swapChain = engine.createSwapChain();
-      this.renderer = engine.createRenderer();
-      this.camera = engine.createCamera();
-      this.view = engine.createView();
-      this.view.setCamera(this.camera);
-      this.view.setScene(this.scene);
-      this.resize();
-      this.render = this.render.bind(this);
-      this.resize = this.resize.bind(this);
-      window.addEventListener('resize', this.resize);
-      window.requestAnimationFrame(this.render);
-  }
-
-  render() {
-      const radians = Date.now() / 1000;
-      const transform = mat4.fromRotation(mat4.create(), radians, [0, 0, 1]);
-      const tcm = this.engine.getTransformManager();
-      const inst = tcm.getInstance(this.triangle);
-      tcm.setTransform(inst, transform);
-      inst.delete();
-      this.renderer.render(this.swapChain, this.view);
-      window.requestAnimationFrame(this.render);
-  }
-
-  resize() {
-      const dpr = window.devicePixelRatio;
-      const width = this.canvas.width = window.innerWidth * dpr;
-      const height = this.canvas.height = window.innerHeight * dpr;
-      this.view.setViewport([0, 0, width, height]);
-      const aspect = width / height;
-      this.camera.setProjection(Projection.ORTHO, -aspect, aspect, -1, 1, 0, 1);
-  }
-}
-
-var f;
-function initFilament()
-{
-  //f = new HelloTriangle();
-  var vb = Filament.VertexBuffer.Builder();
-
-  vb.vertexCount(3);
-  vb.bufferCount(2);
-}
-
-function testBuilder()
-{
-  Filament.testBuilder();
-}
-
 type Props = {};
 export default class App extends Component<Props> {
   render() {
@@ -279,9 +187,7 @@ export default class App extends Component<Props> {
       <View style={styles.container}>        
         <FilamentView style={styles.viewport}></FilamentView>
         <View style={styles.buttons}>
-          <Button style={styles.btn} onPress={testBindings} title="test bindings"/>
-          <Button style={styles.btn} onPress={initFilament} title="init filament"/>
-          <Button style={styles.btn} onPress={testBuilder} title="test builder"/>
+          <Button style={styles.btn} onPress={testBindings} title="test bindings"/>          
         </View>
       </View>
     );
