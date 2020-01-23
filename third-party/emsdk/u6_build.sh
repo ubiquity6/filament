60d76617611dd160c9f0d77679b954827c38d759
Sahn Lam <slam@users.noreply.github.com>
2020-01-23 13:30:10 -0800

    Update emsdk version

--- third-party/emsdk/u6_build.sh
+++ third-party/emsdk/u6_build.sh
@@ -3,5 +3,5 @@ set -e
 cd "$(dirname "$0")"
 
 #./emsdk update
-./emsdk install sdk-1.38.28-64bit
-./emsdk activate sdk-1.38.28-64bit
+./emsdk install sdk-1.39.3-64bit
+./emsdk activate sdk-1.39.3-64bit
