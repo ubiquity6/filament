2e9c2b3238a805506fc657736f8a8f9bf50555ff
Sahn Lam <slam@users.noreply.github.com>
2020-01-23 21:16:15 -0800

    Get filament to build again (#31)
    - Update the buildkite queues to v9
    - Update emsdk to sdk-1.38.33-64bit (sdk-1.38.28-64bit no longer exists)
    - Update EMSCRIPTEN env var (it no longer exists)

--- third-party/emsdk/u6_build.sh
+++ third-party/emsdk/u6_build.sh
@@ -3,5 +3,5 @@ set -e
 cd "$(dirname "$0")"
 
 #./emsdk update
-./emsdk install sdk-1.38.28-64bit
-./emsdk activate sdk-1.38.28-64bit
+./emsdk install sdk-1.38.33-64bit
+./emsdk activate sdk-1.38.33-64bit
