55ea261623d0a6d4946d4621a1a18b00bc47e84c
Sahn Lam <slam@users.noreply.github.com>
2020-01-23 21:53:50 -0800

    Found a better version of emsdk to use

--- third-party/emsdk/u6_build.sh
+++ third-party/emsdk/u6_build.sh
@@ -3,5 +3,5 @@ set -e
 cd "$(dirname "$0")"
 
 #./emsdk update
-./emsdk install sdk-1.38.33-64bit
-./emsdk activate sdk-1.38.33-64bit
+./emsdk install sdk-fastcomp-1.38.30-64bit
+./emsdk activate sdk-fastcomp-1.38.30-64bit
